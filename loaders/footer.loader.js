d90b98108da7129208cbaaa2749951558af7c6c5
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-08-04 16:52:46 -0300

    first-commit

---
+++ loaders/footer.loader.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("./footer/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+    })
+    .catch((error) =>
+      console.error("Não foi possível carregar o footer. Erro:", error)
+    );
+});
---
+++ loaders/header.loader.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("./header/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    })
+    .catch((error) =>
+      console.error("Não foi possível carregar o header. Erro:", error)
+    );
+});
